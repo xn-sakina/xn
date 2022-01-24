cea4e66c1490b857d7fac1800a9e60581f0b64f6
fz6m <59400654+fz6m@users.noreply.github.com>
2022-01-24 21:05:24 +0800

    chore: improve find core-js pkg pos 🍰

--- packages/meta/src/configs/resolve.ts
+++ packages/meta/src/configs/resolve.ts
@@ -3,7 +3,7 @@ import { EXTS } from '../constants'
 import { IConfigChainOpts } from './interface'
 
 const asyncPolyfill = dirname(require.resolve('regenerator-runtime/package'))
-const corejsPolyfill = dirname(require.resolve('core-js'))
+const corejsPolyfill = dirname(require.resolve('core-js/package'))
 
 export const addResolve = ({ config, userConfig }: IConfigChainOpts) => {
   config.resolve.extensions
