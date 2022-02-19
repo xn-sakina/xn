edb645ffed67b0937fd3e14610546d9f5e5bd6e6
fz6m <59400654+fz6m@users.noreply.github.com>
2022-02-19 19:31:35 +0800

    chore: improve swc config 🍭

--- packages/meta/src/configs/module/swc/index.ts
+++ packages/meta/src/configs/module/swc/index.ts
@@ -1,6 +1,8 @@
 import { getPolyfills } from '../../../utils/polyfill/getPolyfills'
 import { IConfigChainOpts } from '../../interface'
 
+const ENABLE_DECO_FEATURE = true
+
 export const getSwcConfigs = ({ root, envs }: IConfigChainOpts) => {
   const isDev = envs.isDev
 
@@ -24,27 +26,37 @@ export const getSwcConfigs = ({ root, envs }: IConfigChainOpts) => {
              *
              * display inject polyfill
              */
-            include: getPolyfills({ root, env: envs.raw.NODE_ENV }),
+            include: [
+              ...getPolyfills({ root, env: envs.raw.NODE_ENV }),
+              // we always transpile optional chaining and nullish coalescing
+              // since it can cause issues with webpack even if the node target
+              // supports them
+              'proposal-optional-chaining',
+              'proposal-nullish-coalescing-operator',
+            ],
           },
         }),
     jsc: {
       parser: {
         syntax: 'typescript',
         dynamicImport: true,
-        decorators: true,
+        decorators: ENABLE_DECO_FEATURE,
         tsx: true,
       },
-      loose: true, // works like babel-preset-env loose mode.
-      target: 'es2015',
+      // works like babel-preset-env loose mode
+      loose: true,
+      target: 'es5',
+      // not use @swc/helper
       externalHelpers: false,
       transform: {
-        legacyDecorator: true,
-        decoratorMetadata: true,
+        legacyDecorator: ENABLE_DECO_FEATURE,
+        decoratorMetadata: ENABLE_DECO_FEATURE,
         react: {
           runtime: 'automatic', // classic
           throwIfNamespace: true,
           useBuiltins: true, // Use Object.assign() instead of _extends. Defaults to false.
           development: isDev,
+          refresh: true,
         },
       },
     },
