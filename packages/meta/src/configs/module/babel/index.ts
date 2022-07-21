7f5581da566fe4fc5dd7898b278ec5b3b111166c
fz6m <59400654+fz6m@users.noreply.github.com>
2022-07-21 15:03:01 +0800

    chore: babel not only remove types 🍰

--- packages/meta/src/configs/module/babel/index.ts
+++ packages/meta/src/configs/module/babel/index.ts
@@ -25,7 +25,6 @@ export const getBabelConfig = ({ envs }: IConfigChainOpts) => {
         {
           allowNamespaces: true,
           allowDeclareFields: true,
-          onlyRemoveTypeImports: true,
           optimizeConstEnums: true,
         },
       ],
