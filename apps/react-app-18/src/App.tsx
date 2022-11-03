cc3454bb0b16968ba271734259d3046a1b1a1799
fz6m <59400654+fz6m@users.noreply.github.com>
2022-11-03 21:15:27 +0900

    fix: lightning css cannot find `browserslist` 🍡

--- apps/react-app-18/src/App.tsx
+++ apps/react-app-18/src/App.tsx
@@ -4,7 +4,9 @@ import { ConfigProvider as AntdConfigProvider } from "antd";
 import { ConfigProvider } from "@arco-design/web-react";
 import "@arco-design/web-react/dist/css/arco.css";
 import zhCN from "@arco-design/web-react/es/locale/zh-CN";
-import Page from "./Page";
+import React from "react";
+
+const Page = React.lazy(() => import("./Page"));
 
 const queryClient = new QueryClient();
 
@@ -13,7 +15,9 @@ export default function App() {
     <AntdConfigProvider>
       <ConfigProvider locale={zhCN}>
         <QueryClientProvider client={queryClient}>
-          <Page />
+          <React.Suspense fallback={<div></div>}>
+            <Page />
+          </React.Suspense>
         </QueryClientProvider>
       </ConfigProvider>
     </AntdConfigProvider>
--- apps/react-app-18/src/Page.tsx
+++ apps/react-app-18/src/Page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "antd";
 import { Button as ArcoButton } from "@arco-design/web-react";
+import { add } from "lodash";
 
 export default function Page() {
   return (
     <div>
+      {add(1, 2)}
       <Button>button</Button>
       <ArcoButton>button</ArcoButton>
     </div>
