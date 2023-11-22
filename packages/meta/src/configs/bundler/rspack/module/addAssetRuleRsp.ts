d15c78298205dfdb1acd616491b6302401d40e21
fz6m <59400654+fz6m@users.noreply.github.com>
2023-11-22 22:40:17 +0800

    chore: remove rspack svgr unused code 🌸

--- packages/meta/src/configs/bundler/rspack/module/addAssetRuleRsp.ts
+++ packages/meta/src/configs/bundler/rspack/module/addAssetRuleRsp.ts
@@ -1,18 +1,6 @@
-import { RSPACK_CONST } from '../../../../constants'
 import { IRspContext } from '../interface'
 
-export const addAssetRuleRsp = ({ config, rawConfig, opts }: IRspContext) => {
-  const cssRule = rawConfig.module!.rules![1]
-  const useSvgr = opts.userConfig.svgr
-  if (useSvgr) {
-    // @ts-expect-error
-    cssRule.oneOf.some((rule) => {
-      if (rule.test?.test?.('a.svg')) {
-        // svgr
-        rule.type = RSPACK_CONST.type.tsx
-        return true
-      }
-    })
-  }
-  config.module!.rules!.push(cssRule as any)
+export const addAssetRuleRsp = ({ config, rawConfig }: IRspContext) => {
+  const assetRule = rawConfig.module!.rules![1]
+  config.module!.rules!.push(assetRule as any)
 }
